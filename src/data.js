102d2875981b6cf59c52e056285f3e1913290e56
tajahmed <m.t.a.78692@gmail.com>
2021-01-19 18:46:25 +0530

    menu my

---
+++ src/data.js
@@ -0,0 +1,77 @@
+const menu = [
+
+  {
+    id: 1,
+    title: 'Yummy yipeeeee',
+    category: 'breakfast',
+    price: 79,
+    img: './images/item-2.jpeg',
+    desc: `Cheesy yummy yipee noodles to rejuvenate your taste buds.`,
+
+  },
+  {
+    id: 2,
+          title: 'Chicken Manchurian',
+          category: 'lunch',
+          price: 145,
+    img: './images/item-5.jpeg',
+    desc: `Deep fried chicken balls, cooked in a gingery, garlicky gravy with soy sauce, chilli sauce and a lot more!`,
+  },
+  {
+    id: 3,
+    title: 'hyderabadi dum biryani',
+    category: 'lunch',
+    price: 250,
+    img: './images/item-1.jpeg',
+    desc: `Crafted in Hyderabadi style, tender chicken pieces are dum-pukht with aromatic rice & excuberant bhuna spices in full glory. [Spicy & boneless, served with 6 kala Jamun & Mint Raita]`,
+  },
+  {
+    id: 4,
+          title: 'Mango Shales',
+          category: 'shakes',
+          price: 49,
+    img: './images/item-9.jpeg',
+    desc: `The naturally irresistible sweetness of alphonso mangoes blended with thick, fresh milk.`,
+  },
+  {
+    id: 5,
+    title: 'Fruit Salad',
+    category: 'breakfast',
+    price: 105,
+    img: './images/item-3.jpeg',
+    desc: `Pretty and colourful bowl made from a luxurious mix of exotic tropical fruits kiwi, dragon fruit balls, strawberry, australian grape, apple, pomo, papaya etc.`,
+  },
+  {
+    id: 6,
+          title: 'Chicken Pakora',
+          category: 'Evening time',
+          price: 99,
+    img: './images/item-6.jpeg',
+    desc: `Our very own tea time snack made by deep-frying sliced boneless chicken in a gluten-free and vegan chickpea flour batter.`,
+  },
+  {
+    id: 7,
+          title: 'Irani Chai (Flask serve 2-3)',
+          category: 'breakfast',
+          price: 120,
+    img: './images/item-7.jpeg',
+    desc: `Rich, Dense Milk Slow-Boiled With Caramelised Sugar. Made With Fresh Milk And The Best Tea Leaves From Assam.`,
+  },
+  {
+    id: 8,
+          title: 'Binge Two-gether Meals for NoN-veg + Veg',
+          category: 'lunch',
+          price: 230,
+    img: './images/item-8.jpeg',
+    desc: `1 Delicious Kadai chicken rice bowl + 1 Protein Power Paneer Tikka Rice Bowl.`,
+  },
+  {
+    id: 9,
+          title: 'Chicken Pizza - IPL Party Pack ',
+          category: 'Evening time',
+          price: 199,
+    img: './images/item-4.jpeg',
+    desc: `10", Barbeque chicken, chicken sausages, onions, red paprika, smoked chicken, chicken salami & peri peri dip (serves 1).`,
+  },
+];
+export default menu;
